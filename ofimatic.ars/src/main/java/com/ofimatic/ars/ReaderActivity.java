b01904857692faab8b74511871e7650fe7e01f97
mmartinez-ofimatic <mmartinez@ofimatic.com>
2014-03-28 09:55:25 -0400

    -Actividades Creadas -Libreria Agregada

--- ofimatic.ars/src/main/java/com/ofimatic/ars/ReaderActivity.java
+++ ofimatic.ars/src/main/java/com/ofimatic/ars/ReaderActivity.java
@@ -6,19 +6,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class ReaderActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_reader);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.reader, menu);
         return true;
     }
 
